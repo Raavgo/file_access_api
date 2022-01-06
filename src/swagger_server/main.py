9f5b7660e89fdfe76b930b35ecd97def7e87aed5
kedaron <kedar@gmx.at>
2022-01-06 22:28:36 +0100

    adjusted get, post and delete in main.py; adjusted template_main.html

--- src/swagger_server/main.py
+++ src/swagger_server/main.py
@@ -1,50 +1,50 @@
 import os
 
-from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = './files'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "super secret key unique"
 uploads = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+def main_page():
+    files = list()
+    for file in os.listdir(UPLOAD_FOLDER):
+        files.append(str(file))
+
+    return render_template('all_files.html', files=files)
+
+@app.route('/rest/api/v1/file', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+            return {"status": "No file part"}, 404
+
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
+
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
+            return {"status": "No selected file"}, 404
+
+        if file:
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             #return redirect(url_for('download_file', name=filename))
 
-        files = list()
-        for file in os.listdir(UPLOAD_FOLDER):
-            files.append(str(file))
+        return {"status": "success"}, 200
 
-        return render_template('all_files.html', files=files)
+@app.route('/rest/api/v1/file', methods=['GET'])
+def download_file():
+    name = request.args.get('name')
+    return send_from_directory(uploads, name)
 
-    if request.method == 'GET':
-
-        return send_from_directory(uploads, "PErmission.png")
-
-@app.route('/', methods=['DELETE'])
+@app.route('/rest/api/v1/file', methods=['DELETE'])
 def delete():
-    name = "PErmission.png"
+    name = request.args.get('name')
     os.chdir(uploads)
     os.remove(name)
     os.chdir(app.root_path)
@@ -52,7 +52,7 @@ def delete():
     for file in os.listdir():
         files.append(str(file))
 
-    return render_template('all_files.html', files=files)
+    return {"status": "success"}, 200
 
 
 if __name__ == "__main__":

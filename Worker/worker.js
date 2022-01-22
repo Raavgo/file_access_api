92bb42f7de7bc4abd6e7cda849d536c3394c3906
Antonia <ai21m032@technikum-wien.at>
2022-01-22 16:26:56 +0100

    Added Videoshow and image logic

--- Worker/worker.js
+++ Worker/worker.js
@@ -1,19 +1,10 @@
-//var videoshow = require('videoshow')
+var videoshow = require('videoshow')
 const axios = require('axios');
 //const express = require('express')
 const amqp = require('amqplib/callback_api');
 const { response } = require('express');
-
-
-//const app = express();
-/*const port = 8001;
-
-const conn_options = {
-  server:     'http://api:5000',
-
-  get_all:    '/rest/api/v1/file/all',
-  api_path:   '/rest/api/v1/file'
-};*/
+var FileReader = require('filereader');
+var filereader = new FileReader();
 
 
 amqp.connect('amqp://0.0.0.0', function(error0, connection) {
@@ -41,95 +32,65 @@ amqp.connect('amqp://0.0.0.0', function(error0, connection) {
             var file_name = splitted[0];
             var video_name = splitted[1];
 
+            var images = [];
 
-            var images;
-            var video;
-            
+            //Get all file names
             axios
               .get(`http://127.0.0.1:5000/rest/api/v1/file/all`, {
-              })
-              .then(res => res.blob())
-              .then(imageBlob => {
-                const imageObjectURL = URL.createObjectURL(imageBlob);
-                console.log(imageObjectURL);
-              })
-              .catch(error => {
-                console.error(error)
-              })
-
-              axios
-              .get(`http://127.0.0.1:5000/rest/api/v1/file?name=${video_name}`, {
+                responeType: 'json',
+                transformResponse: [v => v]
               })
               .then(res => {
                 console.log(`statusCode: ${res.status}`)
-                video = res.data;
+                console.log(`Data is: ${res.data}`)
+
+                var obj = JSON.parse(res.data);
+
+                for(var image of Object.values(obj)){
+                  //Get images
+                  images.push(`http://127.0.0.1:5000/rest/api/v1/file?name=${image}`);
+                }
+                
+                console.log(images);
+                generateVideo(images);
+                console.log("help");
               })
               .catch(error => {
                 console.error(error)
               })
-
-              if (video == null)
-              {
-                console.log("Video is null");
-              }
-
-
-
         }, {
             noAck: true
         });
     });
 });
 
-
-/*
-
-app.get('/generate', (req, res)=>{
-  axios.get(conn_options.server + conn_options.get_all)
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-*/
-/*
-var images = [
-  'Step 0.png',
-  'Step 1.png',
-  'Step 2.png',
-  'Step 3.png',
-  'Step 4.png'
-]
-
-var videoOptions = {
-  fps: 25,
-  loop: 5, // seconds
-  transition: true,
-  transitionDuration: 1, // seconds
-  videoBitrate: 1024,
-  videoCodec: 'libx264',
-  size: '640x?',
-  audioBitrate: '128k',
-  audioChannels: 2,
-  format: 'mp4',
-  pixelFormat: 'yuv420p'
+function generateVideo(images){
+  var videoOptions = {
+    fps: 25,
+    loop: 5, // seconds
+    transition: true,
+    transitionDuration: 1, // seconds
+    videoBitrate: 1024,
+    videoCodec: 'libx264',
+    size: '640x?',
+    audioBitrate: '128k',
+    audioChannels: 2,
+    format: 'mp4',
+    pixelFormat: 'yuv420p'
+  }
+  
+  videoshow(images, videoOptions)
+    .save('video.mp4')
+    .on('start', function (command) {
+      console.log('ffmpeg process started:', command)
+    })
+    .on('error', function (err, stdout, stderr) {
+      console.error('Error:', err)
+      console.error('ffmpeg stderr:', stderr)
+    })
+    .on('end', function (output) {
+      console.error('Video created in:', output)
+    })
 }
 
-videoshow(images, videoOptions)
-  .save('video.mp4')
-  .on('start', function (command) {
-    console.log('ffmpeg process started:', command)
-  })
-  .on('error', function (err, stdout, stderr) {
-    console.error('Error:', err)
-    console.error('ffmpeg stderr:', stderr)
-  })
-  .on('end', function (output) {
-    console.error('Video created in:', output)
-  })*/
+
